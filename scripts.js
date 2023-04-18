9bb5fbfa49e3898de5d42e7a05b1ee4525902f14
phuong <khanh-phuong.NGUYEN@etu.univ-amu.fr>
2023-04-18 22:53:50 +0200

    Phuong

--- scripts.js
+++ scripts.js
@@ -1,11 +1,10 @@
 const { exec } = require('child_process');
 const { readdirSync } = require('fs');
 
-const folder1Files = readdirSync('./gestion_HR').filter(file => file.endsWith('.js'));
-const folder2Files = readdirSync('./gestionChambre').filter(file => file.endsWith('.js'));
+const folderFiles = readdirSync('./gestion').filter(file => file.endsWith('.js'));
 
-folder1Files.forEach(file => {
-  exec(`node ./gestion_HR/${file}`, (err, stdout, stderr) => {
+folderFiles.forEach(file => {
+  exec(`node ./gestion/${file}`, (err, stdout, stderr) => {
     if (err) {
       console.error(`Error running ${file}: ${err}`);
       return;
@@ -14,12 +13,3 @@ folder1Files.forEach(file => {
   });
 });
 
-folder2Files.forEach(file => {
-  exec(`node ./gestionChambre/${file}`, (err, stdout, stderr) => {
-    if (err) {
-      console.error(`Error running ${file}: ${err}`);
-      return;
-    }
-    console.log(`${file} ran successfully`);
-  });
-});
